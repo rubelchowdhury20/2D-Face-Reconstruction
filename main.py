a79d67259b5b743c59a043002e90097d6cb30f03
rubel <rubelchowdhury20@gmail.com>
2018-09-20 18:53:58 +0530

    added the synthetic data download portion

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-Hello
+from src import utils
+
+
+def main(args):
+	utils.download_file()
+
+if __name__ == '__main__':
+	main()
---
+++ src/utils/dataset_utils.py
@@ -0,0 +1,15 @@
+import os
+import gdown
+
+
+
+def download_file():
+	synthetic_path = "./../../data/synthetic_data"
+	if not os.path.exists(synthetic_path):
+	    os.makedirs(synthetic_path)
+
+
+	synthetic_url = 'https://drive.google.com/uc?id=18YVh0idJ9tNgrYluDJqsE3oY_dWVIJBE'
+	print("download started")
+	gdown.download(synthetic_url, synthetic_path)
+	print("download completed")
