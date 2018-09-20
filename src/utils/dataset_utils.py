dd1fece5eede37c9a50aa2836163b0d13880d913
rubel <rubelchowdhury20@gmail.com>
2018-09-20 19:22:44 +0530

    small change

--- src/utils/dataset_utils.py
+++ src/utils/dataset_utils.py
@@ -4,12 +4,12 @@ import gdown
 
 
 def download_file():
-	synthetic_path = "./../../data/synthetic_data"
+	synthetic_path = "./data/synthetic_data"
 	if not os.path.exists(synthetic_path):
 	    os.makedirs(synthetic_path)
 
 
 	synthetic_url = 'https://drive.google.com/uc?id=18YVh0idJ9tNgrYluDJqsE3oY_dWVIJBE'
 	print("download started")
-	gdown.download(synthetic_url, synthetic_path)
+	gdown.download(synthetic_url, synthetic_path, quiet=False)
 	print("download completed")
