277733458fe6bcca6423751543216ad2f6b28d99
Bhagat <Ankita.Bhagat@shell.com>
2018-09-21 00:01:43 +0530

    mask added

--- src/utils/generate_mask.py
+++ src/utils/generate_mask.py
@@ -1,4 +1,15 @@
 import os
+import numpy as np
+import skimage
+from skimage import io
+from skimage import transform
+import cv2
+from scipy.misc.pilutil import imsave
+from imutils import face_utils
+import collections
+import dlib
+from glob import glob
+import shutil
 
 # Listing the path of all the celebA images
 def listdir_fullpath(d):
@@ -24,3 +35,54 @@ def generate_mask():
 	list_img_full=listdir_fullpath('data/CelebA/img_align_celeba/') #change 
 
 	for i, image_path in enumerate(list_img_full):
+
+		if(mask_count == (len(list_img_full) - (len(list_img_full) % skipnet_batch_size))):
+			break
+		# read image
+		image = io.imread(image_path)
+		
+		if i%500 == 0:
+			print("Mask generation for "+ str(i) +" images are done")
+
+		name = image_path.strip().split('/')[-1]
+		gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	  
+
+		out_face = np.zeros((image.shape[0], image.shape[1]))
+		SHAPE_PREDICTOR = 'shape_predictor_68_face_landmarks.dat'		#change
+		# initialize dlib's face detector (HOG-based) and then create the facial landmark predictor
+		detector = dlib.get_frontal_face_detector()
+		predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
+
+		# detect faces in the grayscale image
+		rects = detector(gray, 1)
+		mask_exists = False
+		
+		# loop over the face detections
+		for (i, rect) in enumerate(rects):
+			mask_count+=1
+		"""
+		Determine the facial landmarks for the face region, then convert the facial landmark (x, y)-coordinates to a NumPy array
+		"""
+		shape = predictor(gray, rect)
+		shape = face_utils.shape_to_np(shape)
+
+		#initialize mask array
+		remapped_shape = np.zeros_like(shape)
+
+		feature_mask = np.zeros((image.shape[0], image.shape[1]))
+
+		# we extract the face
+		remapped_shape = face_remap(shape)
+		cv2.fillConvexPoly(feature_mask, remapped_shape[0:27], 1)
+		feature_mask = feature_mask.astype(np.bool)
+		out_face[feature_mask] = gray[feature_mask]
+		out_face = out_face * 255
+		out_face = out_face.reshape(out_face.shape[0], out_face.shape[1], 1)
+		out_face = np.tile(out_face, (1, 1, 3))
+		cv2.imwrite(os.path.join(save_folder, name), out_face)
+		mask_exists = True
+		if mask_exists == False :
+			mask_count-=1
+			shutil.move(image_path, nomask_folder + "/" + name)		#create
+			print("Mask is not created for "+name+". Image is moved out!")
