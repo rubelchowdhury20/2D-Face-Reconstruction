c8307ebff5e3e22ea7f312be5a5767ecefe638a8
shreyasrajesh <shreyas.rajesh1997@gmail.com>
2018-09-22 19:57:25 +0000

    Fixed SFSNet

--- src/models/sfsnet/sfsnet_functions.py
+++ src/models/sfsnet/sfsnet_functions.py
@@ -39,7 +39,44 @@ def generate_list():
                 elif (each_file.endswith(".txt")):
                     celebA_light_list.append(os.path.join(root, each_file))
 
-    features, labels = generate_syn_list()
+    image_list = []
+    normal_list = []
+    albedo_list = []
+    mask_list = []
+    light_list = []
+    if os.path.exists('data/synthetic_data/DATA_pose_15/'):
+        folder_count = len(next(os.walk('data/synthetic_data/DATA_pose_15/'))[1])
+    else:
+        folder_count = 10
+
+    # generating list for image, albedo, normal, lighting SH and mask in a similar order
+    for i in range(0,folder_count):
+        folder = str(i+1).zfill(4)
+        path = 'data/synthetic_data/DATA_pose_15/' + folder + '/'
+        addrs = os.walk(path)
+        for root, dirs, filename in addrs:
+            filename.sort() 
+            for file in filename:
+                if (file.endswith(".png") and 'face' in file):
+                    image_list.append(os.path.join(root, file))       
+                elif (file.endswith(".png") and 'albedo' in file):
+                    albedo_list.append(os.path.join(root, file))
+                elif (file.endswith(".png") and 'normal' in file):
+                    normal_list.append(os.path.join(root, file))
+                elif (file.endswith(".png") and 'mask' in file):
+                    mask_list.append(os.path.join(root, file))         
+                elif (file.endswith(".txt")):
+                    light_list.append(os.path.join(root, file))
+    # converting list to numpy array 
+    image_list = np.asarray(image_list)
+    albedo_list = np.asarray(albedo_list)
+    normal_list = np.asarray(normal_list)
+    light_list = np.asarray(light_list)
+    mask_list = np.asarray(mask_list)
+
+    features = np.transpose(np.asarray([image_list, mask_list]))
+    labels = np.transpose(np.asarray([normal_list, albedo_list, light_list]))
+
     # TODO: Generate the normal, albedo and mask list as output from the generate_syn_list itself. 
     # Setting the labels for synthetic data
     normal_label=[-1 for i in range(len(list(normal_list)))]
@@ -104,9 +141,12 @@ def train_validation_split(features, labels):
     train_max_index = int(n_total * 0.8)
     test_min_index = int((n_total * 0.8))
 
-    train_features = features[:train_max_index]
-    train_labels = labels[:train_max_index]
-    test_features = features[test_min_index:]
-    test_labels = features[test_min_index:]
+    train_features = np.transpose(features[:train_max_index])
+    train_labels = np.transpose(labels[:train_max_index])
+    test_features = np.transpose(features[test_min_index:])
+    test_labels = np.transpose(features[test_min_index:])
 
-    return train_features, train_labels, test_features, test_labels
+    train_data = {'features':train_features, 'labels':train_labels}
+    test_data = {'features':test_features, 'labels':test_labels}
+
+    return train_data, test_data
