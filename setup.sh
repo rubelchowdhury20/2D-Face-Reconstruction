a41350acdd3c1ac683da72d6256d02949f98e0a2
rubel <rubelchowdhury20@gmail.com>
2018-09-24 00:10:32 +0530

    conda installation code added in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-bash Miniconda3-latest-Linux-x86_64.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
+bash ~/miniconda.sh -b -p ~/miniconda 
+rm ~/miniconda.sh
+
 conda env create -f environment.yml
 source activate 3dface
 
