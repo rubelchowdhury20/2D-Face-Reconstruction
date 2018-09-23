70b9b34eb7a7aee8eb27c56b3aa07b43b8ad49b2
rubel <rubelchowdhury20@gmail.com>
2018-09-24 01:31:34 +0530

    cv2 error fixes

--- setup.sh
+++ setup.sh
@@ -2,12 +2,15 @@ wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/
 bash ~/miniconda.sh -b -p ~/miniconda 
 rm ~/miniconda.sh
 
+echo "PATH=/root/miniconda/bin/:$PATH" >> ~/.bashrc
+source ~/.bashrc
+
 conda env create -f environment.yml
 source activate 3dface
 pip install opencv-contrib-python
 apt-get update
 apt-get upgrade
-apt install -y libsm6 libxext6
+apt install -y libsm6 libxext6 libxrender1
 
 mkdir -p ./models/latest/ 
 sudo mkdir -p /tmp/SFSNet/checkpoints/skipnet_checkpoints
