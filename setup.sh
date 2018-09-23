b725384396aab600a666be0d307e450ed99ae885
shreyasrajesh <shreyas.rajesh1997@gmail.com>
2018-09-23 18:26:41 +0000

    softlinks added

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda env create -f environment.yml
 source activate 3dface
 
-mkdir -p /tmp/SFSNet/logs
-mkdir -p /tmp/SFSNet/checkpoints
+mkdir -p ./models/latest/ 
+sudo mkdir -p /tmp/SFSNet/checkpoints/skipnet_checkpoints
+sudo mkdir -p /tmp/SFSNet/checkpoints/sfsnet_checkpoints
 
 python data_loader.py --skipnet_batch_size 10
