e95e00d61131700d2fe4985912f0e28f94ff849b
Shreyas Rajesh <shreyas.rajesh1997@gmail.com>
2018-09-23 19:37:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,7 @@ bash Miniconda3-latest-Linux-x86_64.sh
 conda env create -f environment.yml
 source activate 3dface
 
+mkdir -p /tmp/SFSNet/logs
+mkdir -p /tmp/SFSNet/checkpoints
+
 python data_loader.py --skipnet_batch_size 10
