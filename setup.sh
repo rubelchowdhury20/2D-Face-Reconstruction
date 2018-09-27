f2e59d1514aff7e32caf895564404a22b11f8f3d
Shreyas Rajesh <shreyas.rajesh1997@gmail.com>
2018-09-27 11:21:08 +0530

    Spacing issue

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 #bash ~/miniconda.sh -b -p ~/miniconda 
 #rm ~/miniconda.sh
+
+
 sudo -s
 # The above command will prompt password entry.
 
