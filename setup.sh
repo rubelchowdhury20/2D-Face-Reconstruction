ecc107d2e6f929eea78fb58c05c913a596244e7e
Shreyas Rajesh <shreyas.rajesh1997@gmail.com>
2018-09-27 11:20:37 +0530

    Added sudo command for root access
    User needs to enter the password manually in order to proceed.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,14 @@
 #wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda.sh
 #bash ~/miniconda.sh -b -p ~/miniconda 
 #rm ~/miniconda.sh
+sudo -s
+# The above command will prompt password entry.
 
 export PATH=/root/miniconda/bin/:$PATH
 
 conda env create -f environment.yml
 source activate 3dface
-pip install opencv-contrib-python
+pip3 install opencv-contrib-python
 apt-get -y update
 apt-get -y upgrade
 apt install -y libsm6 libxext6 libxrender1
